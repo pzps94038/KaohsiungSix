c6461a81bb60b99cf7af1c4f9d0994127226456f
pzps94038 <80943379+pzps94038@users.noreply.github.com>
2021-04-07 22:17:12 +0800

    V2.0

--- js/最後作業.js
+++ js/最後作業.js
@@ -1,85 +1,35 @@
-// 網址
-var dataUrl= "https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json"
-// // new一個XMLHttpRequest物件(以此物件的方法進行資料請求) 
-var xhr = new XMLHttpRequest()
-xhr.open('GET',dataUrl, true)
-xhr.send(null);
-xhr.onload = function(){
-    // 轉換json格式並用全域變數
-    data = JSON.parse(this.responseText).result.records;    
-    // 呼叫資料函式
-    upData(data)
-}
+// 抓到預設本地資料跟時間
+var data = JSON.parse(localStorage.getItem('bmiList')) || [];
+var today = new Date();
 // 抓到網頁元件
-var select = document.getElementById('nav');
-var areaName = document.getElementById('areaName');
-var card = document.getElementById('card');
-var btn = document.querySelector('.admin_btn');
-// 預設最多6筆
-var  page = 6;
-// 重複區域陣列
-var areaList = [];
-// 不重複區域陣列
-var area = [];
-// 處理資料
-function upData(item){
-    var len = item.length;    
-    for(var i=0; i<len; i++){
-        areaList.push(item[i].Zone)        
-    }
-    // 過濾重複 value = 變數
-    areaList.forEach(function(value){
-    if(area.indexOf(value) == -1){
-        area.push(value);
-    }    
-    });
-    upSelectView(area)
-}
-    // 資料更新select介面
-function upSelectView(item){
-    var len = item.length;
-    var str ='';
-    // 預設值但不能點選
-    str+='<option disabled selected>'+'- - 請選擇行政區- -'+'</option>';
-    str+='<option value="全部地區">'+'全部地區'+'</option>';
-    for (var i=0 ; i<len ; i++){
-        str+='<option value="'+item[i]+'">'+item[i]+'</option>';
-    }
-    select.innerHTML = str;
-    str='';
-    
-}
+var result = document.querySelector('.result');
+var record = document.querySelector('.record');
+// 先刷新本地資料
 
-// select選擇更新介面
-function allView(e){    
-    var option = e.target.value;
-    areaName.textContent = option ;
-    var len = data.length;
-    // 選擇行政區
-    str='';    
-    for(var i=0 ; i<len ; i++){
-        if(data[i].Zone == option){
-            str+='<li class="card_item"><div class="item_img"><img src="'+data[i].Picture1+'"><div class="item_img_text"><div class="itemName">'+data[i].Name+'</div><div class="itemZone">'+data[i].Zone+'</div></div></div><div class="item_cotent"><p class="opentime"><img src="image_1/icons_clock.png">'+data[i].Opentime+'</p><p class="adress"><img src="image_1/icons_pin.png">'+data[i].Add+'</p><div class="tel"><div><img src="image_1/icons_phone.png"><span class="phone">'+data[i].Tel+'</span></div><div><img src="image_1/icons_tag.png"><span class="free">'+data[i].Ticketinfo+'</span></div></div></div></li>';                
-        }else if( option=='全部地區' ){
-            str+='<li class="card_item"><div class="item_img"><img src="'+data[i].Picture1+'"><div class="item_img_text"><div class="itemName">'+data[i].Name+'</div><div class="itemZone">'+data[i].Zone+'</div></div></div><div class="item_cotent"><p class="opentime"><img src="image_1/icons_clock.png">'+data[i].Opentime+'</p><p class="adress"><img src="image_1/icons_pin.png">'+data[i].Add+'</p><div class="tel"><div><img src="image_1/icons_phone.png"><span class="phone">'+data[i].Tel+'</span></div><div><img src="image_1/icons_tag.png"><span class="free">'+data[i].Ticketinfo+'</span></div></div></div></li>'; 
+function Bmi(){
+    // 抓到裡面的值
+    var height = document.getElementById('height').value;
+    var kg = document.getElementById('kg').value;
+    var bmi = kg/((height*0.01)*(height*0.01));
+    var todo ={
+        content:bmi,
+        bodyWeight:kg,
+        bodyHeight:height,
+        year = today.getFullYear(),
+        month = today.getMonth(),
+        date = today.getDate(),
+    };
+    data.push(todo);
+    localStorage.setItem('bmiList',JSON.stringify(data));
+    updata(data)
+}
+function updata(item){
+    var len = item.length;
+    str-'';
+    for(var i=0 ; i<length ; i++){
+        if(data[i].content < 16){
+            str+='<li class="Too_light">過輕<p>BMI</p><span>'+item[i].content+'</span><p>weight</p><span>'+item[i].bodyWeight+'</span><p>height</p><span>'+item[i].bodyHeight+'</span><span>'+item[i].date+'-'+item[i].month+'-'+item[i].year;
         }
-    }
-        card.innerHTML = str;
+    }record.innerHTML = str;
 }
-// btn選擇更新介面
-function btnView(e){
-    if( e.target.nodeName != 'INPUT'){return}
-    var option = e.target.value;
-    areaName.textContent = option ;
-    var len = data.length;
-    str='';    
-    for(var i=0 ; i<len ; i++){
-        if(data[i].Zone == option){
-            str+='<li class="card_item"><div class="item_img"><img src="'+data[i].Picture1+'"><div class="item_img_text"><div class="itemName">'+data[i].Name+'</div><div class="itemZone">'+data[i].Zone+'</div></div></div><div class="item_cotent"><p class="opentime"><img src="image_1/icons_clock.png">'+data[i].Opentime+'</p><p class="adress"><img src="image_1/icons_pin.png">'+data[i].Add+'</p><div class="tel"><div><img src="image_1/icons_phone.png"><span class="phone">'+data[i].Tel+'</span></div><div><img src="image_1/icons_tag.png"><span class="free">'+data[i].Ticketinfo+'</span></div></div></div></li>';                
-        }    
-    }
-    card.innerHTML = str;
-}
-// 監聽
-select.addEventListener('change',allView,false);
-btn.addEventListener('click',btnView,false);
+result.addEventListener('click',Bmi,false);
